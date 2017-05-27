402bb256c7c3063c44aaedd6aa1eac99de52872a
windok <windok.ua@gmail.com>
2017-05-27 15:49:08 +0300

    Add ability to make CORS requests to api

--- scripts/ssl_generator.sh
+++ scripts/ssl_generator.sh
@@ -1,1 +1,3 @@
-      openssl req -newkey rsa:2048 -x509 -nodes -keyout server.key -new -out server.crt -subj /CN=pepper-helper.dev -reqexts SAN -extensions SAN -config <(cat /etc/ssl/openssl.cnf <(printf '[SAN]\nsubjectAltName=DNS:pepper-helper.dev')) -sha256 -days 3650
+openssl req -newkey rsa:2048 -x509 -nodes -keyout frontend.key -new -out frontend.crt -subj /CN=pepper-helper.dev -reqexts SAN -extensions SAN -config <(cat /etc/ssl/openssl.cnf <(printf '[SAN]\nsubjectAltName=DNS:pepper-helper.dev')) -sha256 -days 3650
+
+openssl req -newkey rsa:2048 -x509 -nodes -keyout backend.key -new -out backend.crt -subj /CN=api.pepper-helper.dev -reqexts SAN -extensions SAN -config <(cat /etc/ssl/openssl.cnf <(printf '[SAN]\nsubjectAltName=DNS:api.pepper-helper.dev')) -sha256 -days 3650
